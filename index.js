93560be00b40367b1cc21981e6cc5f77a41dbaaa
nicolemenezes <nicmenezes35@gmail.com>
2026-03-15 10:28:16 +0530

    changes

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-const http = require('http');
-const express = require('express');
-const path = require('path');
-const { Server } = require('socket.io');
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  serveClient: true
-});
+const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-io.on('connection', (socket) => {
-  console.log('a new user has connected', socket.id);
-
-  socket.on('chat message', (msg) => {
-    console.log('a new user message', msg);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected', socket.id);
+// Socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
 });
+
+server.listen(9000, () => console.log(`Server Started at PORT:9000`));
